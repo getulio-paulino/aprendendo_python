9964c6b381860ebfc02704117ee5bf4a3623c2c1
getulio-paulino <gepaulpm@gmail.com>
2020-01-14 15:21:24 -0300

    commit após fazer exercicio2

---
+++ exercicio2/ex2.py
@@ -0,0 +1,9 @@
+# um comentário, assim você pode ler sem programa mais tarde.
+# Qualquer cosia depois do # é ignorado pelo python.
+
+print("I could have code like this.") # e o comentário depois é ignorado
+
+# Você também pode usar um comenário para "desabilitar" um código.
+#print("This won't run.")
+
+print("This will run.")
